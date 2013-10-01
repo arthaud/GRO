50024714b27f1054e87de62f93db8e17fed7645e
Carton Martin <mcarton@phosphore.enseeiht.fr>
2013-10-01 15:58:10 +0200

    Ajout version améliorée de hamiltonian_path

--- hamiltonien.py
+++ hamiltonien.py
@@ -9,7 +9,7 @@ def is_hamiltonian(graph):
         return True
 
     # general test, complexity sucks
-    return hamiltonian_path(graph) != None
+    return hamiltonian_path2(graph) != None
 
 def hamiltonian_path(graph, node_from=None, nodes_done=frozenset()):
     if node_from is None:
@@ -19,6 +19,33 @@ def hamiltonian_path(graph, node_from=None, nodes_done=frozenset()):
 
     if len(nodes_done) == graph.order():
         return [node_from]
+
+    for edge in node_from.edges_out:
+        other = edge.other_side(node_from)
+        if other in nodes_done:
+            continue
+        path = hamiltonian_path(graph, other, nodes_done)
+        if path:
+            return [node_from] + path
+
+    return None
+
+def hamiltonian_path2(graph, node_from=None, nodes_done=frozenset()):
+    if node_from is None:
+        node_from = graph.nodes[0]
+
+    nodes_done = nodes_done | frozenset((node_from,))
+
+    if len(nodes_done) == graph.order():
+        return [node_from]
+
+    poss = filter(lambda x: x.other_side(node_from).degree() == 1, node_from.edges_out)
+
+    if len(poss) == 1:
+        path = hamiltonian_path(graph, poss[0].other_side(node_from), nodes_done)
+        return [node_from] + path if path else None
+    elif len(poss) > 1:
+        return None
 
     for edge in node_from.edges_out:
         other = edge.other_side(node_from)
