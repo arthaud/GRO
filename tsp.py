caeb1d4c463c4504c3758f1c393b8b23415cbf9d
Martin Carton <cartonmartin+git@gmail.com>
2013-10-01 22:07:02 +0200

    TSP doit retourner au point de départ

--- tsp.py
+++ tsp.py
@@ -3,7 +3,7 @@
 
 import sys
 
-def nearest_neighbor(graph, node_from=None, nodes_done=frozenset()):
+def nearest_neighbor(graph, node_from=None, first_node=None, nodes_done=frozenset()):
     """
     Nearest Neighbor algorithm, simple approximation for TSP problem.
     Requires the graph to be complete.
@@ -13,10 +13,19 @@ def nearest_neighbor(graph, node_from=None, nodes_done=frozenset()):
     if node_from is None:
         node_from = graph.nodes[0]
 
+    if first_node is None:
+        first_node = node_from
+
     nodes_done = nodes_done | frozenset((node_from,))
 
     if len(nodes_done) == graph.order():
-        return (0, [node_from])
+        return_distance = 0
+        for edge in first_node.edges_out:
+            if edge.other_side(node_from) == first_node:
+                return_distance = edge.cost
+                break
+
+        return (return_distance, [node_from, first_node])
 
     closest = None
     closest_distance = sys.maxint
@@ -27,5 +36,5 @@ def nearest_neighbor(graph, node_from=None, nodes_done=frozenset()):
             closest = other
             closest_distance = edge.cost
 
-    path_end = nearest_neighbor(graph, closest, nodes_done)
+    path_end = nearest_neighbor(graph, closest, first_node, nodes_done)
     return (closest_distance + path_end[0], [node_from] + path_end[1])
