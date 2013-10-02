62cb9e43b53c91683e6ebc177616f298e8485161
Martin Carton <cartonmartin+git@gmail.com>
2013-10-02 16:37:21 +0200

    Ajouté algo 2-opt, à tester sur un grand graph

--- tsp.py
+++ tsp.py
@@ -19,6 +19,7 @@ def nearest_neighbor(graph, node_from=None, first_node=None, nodes_done=frozense
     nodes_done = nodes_done | frozenset((node_from,))
 
     if len(nodes_done) == graph.order():
+
         return (node_from.cost_to(first_node), [node_from, first_node])
 
     closest = None
@@ -32,3 +33,27 @@ def nearest_neighbor(graph, node_from=None, first_node=None, nodes_done=frozense
 
     path_end = nearest_neighbor(graph, closest, first_node, nodes_done)
     return (closest_distance + path_end[0], [node_from] + path_end[1])
+
+def two_opt(solution, max_iteration):
+    """
+        2-opt algorithm, try to find a better solution than a given one.
+    """
+
+    best_cost, best_path = solution
+
+    for iterations in range(1, max_iteration):
+        got_better_solution = False
+        for i in range(1, len(best_path)-2):
+            # [..., a, b, c, d, ...] -> [..., a, c, b, d, ...]
+            a, b, c, d = (best_path[j] for j in range(i-1, i+3))
+            new_cost = best_cost - a.cost_to(b) - c.cost_to(d) + a.cost_to(c) + b.cost_to(d)
+
+            if new_cost < best_cost:
+                got_better_solution = True
+                best_cost = new_cost
+                best_path = best_path[:i] + [c, b] + best_path[i+2:]
+
+        if not got_better_solution:
+            return None 
+
+    return None if best_cost == solution[0] else (best_cost, best_path)
