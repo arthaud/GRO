b7710d8383f1ded39fad31d4a1a2d666846f2f6a
Martin Carton <cartonmartin+git@gmail.com>
2013-10-02 19:46:31 +0200

    Ajout tests pour is_hamiltonian et de tests sur de gros fichiers pour vérifier qu'ils tiennent le coup

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 from graphs import Graph, Node, Edge
-import hamiltonien
+from hamiltonian import *
 from eulerian import *
+from tsp import *
 
 def print_ok(string):
     print "\033[92m" + string + "\033[0m"
@@ -19,32 +20,31 @@ def test(condition, tested_fn, graph_nb):
     else:
         print_err("Error testing %s on graph number %s" % (tested_fn, graph_nb))
 
-if __name__ == '__main__':
-    graphs = []
-
-    # Format: [Graph, connected, eulerian, semi-eulerian]
-    graphs.append([Graph('tests/1.gph'), True, True, False])
-    graphs.append([Graph('tests/2.gph'), True, False, True])
-    graphs.append([Graph('tests/3.gph'), False, False, False])
-    graphs.append([Graph('tests/4.gph'), True, False, False])
-
-    #tests connexité
-    tested_fn = "Graph.is_connected"
+def test_one(graphs, fun, tested_fn):
     i = 0
     for g in graphs:
         i = i + 1
         test(g[0].is_connected() == g[1], tested_fn, i)
 
+if __name__ == '__main__':
+    graphs = []
+
+    # Format: [Graph, connected, eulerian, semi-eulerian, hamiltonian]
+    graphs.append([Graph('tests/1.gph'), True, True, False, True])
+    graphs.append([Graph('tests/2.gph'), True, False, True, False])
+    graphs.append([Graph('tests/3.gph'), False, False, False, False])
+    graphs.append([Graph('tests/4.gph'), True, False, False, True])
+    graphs.append([read_tsp('tests/berlin52.tsp'), True, False, None, True])
+    graphs.append([read_tsp('tests/d657.tsp'), True, False, None, True])
+
+    #tests connexité
+    #test_one(graphs, is_connected, "Graph.is_connected")
+
     #tests eulérianité
-    tested_fn = "is_eulerian"
-    i = 0
-    for g in graphs:
-        i = i + 1
-        test(is_eulerian(g[0]) == g[2], tested_fn, i)
+    test_one(graphs, is_eulerian, "Graph.is_eulerian")
 
     # tests semi eulerianité
-    tested_fn = "is_semi_eulerian"
-    i = 0
-    for g in graphs:
-        i = i + 1
-        test(is_eulerian(g[0]) == g[2], tested_fn, i)
+    test_one(graphs, is_semi_eulerian, "Graph.is_semi_eulerian")
+
+    # tests hamiltonian
+    test_one(graphs, is_hamiltonian, "Graph.is_hamiltonian")
