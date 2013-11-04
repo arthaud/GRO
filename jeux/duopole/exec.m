e456792ec4457cf7b308c641c4f6346e66415ae4
Martin Carton <cartonmartin+git@gmail.com>
2013-11-04 19:19:33 +0100

    recode ISO-8859-15..UTF8 jeux/duopole/*

--- jeux/duopole/exec.m
+++ jeux/duopole/exec.m
@@ -1,9 +1,9 @@
 disp('Duopole de Cournot');
-disp('Stratégies possibles : cooperatif/noncooperatif/stackelberg');
+disp('StratÃ©gies possibles : cooperatif/noncooperatif/stackelberg');
 NBJ=input('Nombre de parties: ');
-nomx=input('Stratégie du 1er joueur: ','s');
+nomx=input('StratÃ©gie du 1er joueur: ','s');
 strax=[nomx,'1'];
-nomy=input('Stratégie du second joueur: ','s');
+nomy=input('StratÃ©gie du second joueur: ','s');
 stray=[nomy,'2'];
 [tx, ty, gx, gy] = jeu(NBJ,strax,stray);
 fig = afficher(tx,ty,gx,gy,nomx,nomy);
