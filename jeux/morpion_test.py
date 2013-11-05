017e2c4bad4991714a80313af36d9ff5f53cd971
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2013-11-05 15:37:15 +0100

    Modifié get_strategies pour retourner des fonctions

--- jeux/morpion_test.py
+++ jeux/morpion_test.py
@@ -2,8 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import morpion_strategies
+import sys
 
 def get_strategies():
-    return filter(lambda f : f.startswith("strat") and f != 'strat_humain', dir(morpion_strategies))
+    """
+    Retourne toutes les stratégies disponibles, à l'exception de la stratégie humaine
+    """
+    return map(lambda f : getattr(morpion_strategies, f),filter(lambda f : f.startswith("strat") and f != 'strat_humain', dir(morpion_strategies)))
 
-print get_strategies()
+if __name__ == '__main__':
+    strats = get_strategies()
+    file = open(sys.argv[1], 'w')
+    
+    for i, strat in enumerate(strats):
+        for opponent in strats[i:]:
+            pass
