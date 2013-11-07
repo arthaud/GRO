c0abe0491b8ba180c1bc34fb0023314d793d851f
Martin Carton <cartonmartin+git@gmail.com>
2013-11-07 17:41:15 +0100

    poly est 1% plus efficace \O/

--- jeux/duopole/gklmjbse.m
+++ jeux/duopole/gklmjbse.m
@@ -1,3 +1,4 @@
+% Stratégie joue de plus en plus tant que l'autre coopère et qui coopère sinon.
 function x = strategie(numpart,tx,ty,gx,gy)
 if (numpart <= 2)
     x = 0.75;
@@ -6,10 +7,8 @@ else
 
     if (ty_mean < 0.8)
         x = tanh(numpart/10)*1.5;
-    elseif (ty_mean > 1.3)
-        x = 0.75;
     else
-        x = 1.5;
+        x = 0.75;
     end;
 end;
 
--- jeux/duopole/poly.m
+++ jeux/duopole/poly.m
@@ -3,11 +3,11 @@
 %   f(0.75) = 0.75    (on coopère avec qqn qui coopère)
 %   f(1.5)  = 0.75    (on essaye de coopérer avec qqn qui ne coopère pas, des
 %                      fois qu'il changerait d'avis)
-% en fonction de ty(numpart-1).
+% en fonction de mean(ty(2:numpart-1)).
 function x = strategie(numpart,tx,ty,gx,gy)
-if (numpart == 2)
+if (numpart <= 2)
     x= 0.75;
 else
-    x= 1/3*ty(numpart-1)^2-3/4*ty(numpart-1)+1.125;
+    x= 1/3*mean(ty(2:numpart-1))^2-3/4*mean(ty(2:numpart-1))+1.125;
 end;
 
