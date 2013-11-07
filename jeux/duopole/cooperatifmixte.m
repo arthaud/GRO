293a25e9de52c0a5cfe84419c5a13aa6b219cfb5
Martin Carton <cartonmartin+git@gmail.com>
2013-11-07 17:40:18 +0100

    Rendons à César ce qui est à César

--- jeux/duopole/cooperatifmixte.m
+++ jeux/duopole/cooperatifmixte.m
@@ -1,29 +1,5 @@
+% Stratégie développée par un autre groupe donnée pour les tests.
 function x = strategie(numpart,tx,ty,gx,gy)
-% strategie -- Strategie d'un joueur 
-%
-%  Usage
-%    x = strategie(nx,ny,ng,tx,ty,gx,gy)
-%
-%  Inputs
-%    tx     tableau des strategies jouees par le joueur x
-%    ty     tableau des strategies jouees par le joueur y (l'adversaire)
-%    gx     tableau des gains du joueur x
-%    gy     tableau des gains du joueur y (l'adversaire)
-%
-%  Outputs
-%    x      strategie elaboree par le joueur x
-%
-%  Description
-%    Elaboration d'une strategie dans le cadre de jeux iteres
-%    avec information complete. L'ensemble des strategies est
-%
-%  See also
-%    jeu
-%
-%  References
-%    "L'altruisme perfectionne", J.P. Delahaye, P. Mathieu,
-%    Pour la science No 187, Mai 1993
-%
 
 %cooperation donc on joue d/4, ici d=3 donc x = 0.75
 if(numpart<=8) %on essaye de cooperer pendant 5 tour
--- jeux/duopole/fourbe5.m
+++ jeux/duopole/fourbe5.m
@@ -1,25 +1,5 @@
+% Stratégie développée par un autre groupe donnée pour les tests.
 function x = strategie(numpart,tx,ty,gx,gy)
-% strategie -- Strategie d'un joueur 
-%
-%  Usage
-%    x = strategie(numpart,tx,ty,gx,gy)
-%
-%  Inputs
-%    tx     tableau des strategies jouees par le joueur x
-%    ty     tableau des strategies jouees par le joueur y (l'adversaire)
-%    gx     tableau des gains du joueur x
-%    gy     tableau des gains du joueur y (l'adversaire)
-%
-%  Outputs
-%    x      strategie elaboree par le joueur x
-%
-%  Description
-%    Elaboration d'une strategie dans le cadre de jeux iteres
-%    avec information complete. L'ensemble des strategies est
-%
-%  See also
-%    jeu
-%
 if (numpart == 2)
 	x=0.75;
 else
