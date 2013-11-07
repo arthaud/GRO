0f5069b69aae3fefcf0999dfbc62f24c287838d8
palkeo <palkeo@gmail.com>
2013-11-07 09:31:09 +0100

    Coucou.

--- jeux/duopole/palkeo.m
+++ jeux/duopole/palkeo.m
@@ -1,22 +1,25 @@
 function x = strategie(numpart,tx,ty,gx,gy)
-if(numpart == 1)
-    x = 0.75;
-else
-    x = min(max(ty(numpart - 1), 0.75), 1.5);
 
-    % si la stratégie n'évolue pas sur plusieurs tours, et qu'il est
-    % méchant : j'essaie de communiquer en étant gentil
-    mid_start = floor(numpart / 2);
-    end_start = floor(5 * numpart / 6);
-    if(sum(ty(mid_start : numpart - 1)) / (numpart - mid_start) >= sum(ty(end_start : numpart - 1)) / (numpart - end_start))
-        if(log2(numpart) - floor(log2(numpart)) < 0.1) 
-            x = 0.75;
-        end;
-    end;
+x = min(max(ty(numpart - 1), 0.75), 1.5);
+    
+mid_start = floor(numpart / 2);
+end_start = floor(5 * numpart / 6);
+    
+% anti plongeon dans le négatif
+if(numpart > 5 && mean(ty(numpart - 5 : numpart - 1)) > 2)
+    x = 1;
+end;
 
-    % si on est tous les deux gentils, j'essaie de l'attaquer gentiment
-    % si il réagit, je ferme ma gueule et je coopère.
-    if(numpart > 4 && sum(ty(mid_start : numpart - 1)) / (numpart - mid_start) == 0.75)
-        x = min(tx(numpart - 1) + 0.05, 1.125);
+% si la stratégie n'évolue pas sur plusieurs tours, et qu'il est
+% méchant : j'essaie de communiquer en étant gentil
+if(mean(ty(mid_start : numpart - 1)) >= mean(ty(end_start : numpart - 1)))
+    if(log2(numpart / 10) - floor(log2(numpart / 10)) < 0.1) 
+        x = 0.75;
     end;
+end;
+
+% si on est tous les deux gentils, j'essaie de l'attaquer gentiment
+% si il réagit, je ferme ma gueule et je coopère.
+if(numpart > 4 && mean(ty(mid_start : numpart - 1)) == 0.75)
+    x = min(tx(numpart - 1) + 0.05, 1.125);
 end;
