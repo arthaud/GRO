b7213be11d622127cf0a6c77c490311fbce7c3fa
viod <maxence.ahlouche@gmail.com>
2013-11-12 15:35:09 +0100

    Ajout du répertoire procstochs + squelette fichier promenade_scarabées.py

---
+++ procstochs/promenade_scarabees.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print """
+Usage: promenade_scarabees fichier_graphe nb_scarabees
+"""
