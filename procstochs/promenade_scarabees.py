f800693bff7f011e5ddbff6f0f58bde9d79fbc0f
Maxime Arthaud <maxime.arthaud@gmail.com>
2013-11-18 10:50:53 +0100

    Ajout d'une matrice d'exemple pour les scarabées

--- procstochs/promenade_scarabees.py
+++ procstochs/promenade_scarabees.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-
+import numpy
 import sys
 
+def verifier_matrice(a):
+    assert all(sum(line) == 1.0 for line in a)
+
+exemple = numpy.array([
+    [0.0, 0.6, 0.2, 0.0, 0.2, 0.0],
+    [0.6, 0.0, 0.4, 0.0, 0.0, 0.0],
+    [0.2, 0.4, 0.0, 0.4, 0.0, 0.0],
+    [0.0, 0.0, 0.4, 0.0, 0.4, 0.2],
+    [0.2, 0.0, 0.0, 0.4, 0.0, 0.4],
+    [0.0, 0.0, 0.0, 0.2, 0.4, 0.4],
+])
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print """
 Usage: promenade_scarabees fichier_graphe nb_scarabees
 """
 
-# That file is soooo cool, and soooo complex :D
+    verifier_matrice(exemple)
