dc78a2d65a3f8fe1ff8964e3a21b3d571bcfb445
Maxime Arthaud <maxime.arthaud@gmail.com>
2013-11-18 11:16:38 +0100

    Ajout d'une fonction generer_dot

--- procstochs/promenade_scarabees.py
+++ procstochs/promenade_scarabees.py
@@ -4,7 +4,23 @@ import numpy
 import argparse
 
 def verifier_matrice(a):
-    assert all(sum(line) == 1.0 for line in a)
+    assert a.shape[0] == a.shape[1] # matrice carrée
+    assert all(sum(line) == 1.0 for line in a) # somme d'une ligne = 1
+
+def generer_dot(a):
+    '''
+    Retourne le contenu d'un .dot représentant le graphe représenté par la matrice a
+    '''
+    s = 'digraph Graphe {\n'
+    n = a.shape[0]
+
+    for i in range(n):
+        for j in range(n):
+            if a[i,j] > 0:
+                s += '\t%s -> %s [label=%s]\n' % (chr(ord('A') + i), chr(ord('A') + j), a[i,j])
+
+    s += '}'
+    return s
 
 exemple = numpy.array([
     [0.0, 0.6, 0.2, 0.0, 0.2, 0.0],
