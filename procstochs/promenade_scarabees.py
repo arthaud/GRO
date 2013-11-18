bd41646b89ee04fc38f4c98d4f42ccc47baeddb6
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2013-11-18 11:25:03 +0100

    On lit maintenant tout depuis des fichiers

--- procstochs/promenade_scarabees.py
+++ procstochs/promenade_scarabees.py
@@ -3,21 +3,33 @@
 import numpy
 import sys
 
+class Graphe:
+    def __init__(self, fichier):
+        self.sommets = []
+        for sommet in open(fichier, 'r').readlines():
+            self.sommets.append([int(i) for i in sommet.split(' ')])
+
+class Scarabee:
+    def __init__(self, graphe, fichier_probas):
+        self.graphe = graphe
+        self.probas = []
+
+        for ligne in open(fichier_probas, 'r').readlines():
+            self.probas.append([float(i) for i in ligne.split(' ')])
+
+        assert len(self.probas) == len(graphe.sommets), 'La matrice de probabilité ne correspond pas au graphe !'
+        verifier_matrice(self.probas)
+
 def verifier_matrice(a):
     assert all(sum(line) == 1.0 for line in a)
 
-exemple = numpy.array([
-    [0.0, 0.6, 0.2, 0.0, 0.2, 0.0],
-    [0.6, 0.0, 0.4, 0.0, 0.0, 0.0],
-    [0.2, 0.4, 0.0, 0.4, 0.0, 0.0],
-    [0.0, 0.0, 0.4, 0.0, 0.4, 0.2],
-    [0.2, 0.0, 0.0, 0.4, 0.0, 0.4],
-    [0.0, 0.0, 0.0, 0.2, 0.4, 0.4],
-])
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 2:
         print """
-Usage: promenade_scarabees fichier_graphe nb_scarabees
+Usage: promenade_scarabees fichier_graphe fichier_proba fichier_proba...
 """
-
-    verifier_matrice(exemple)
+    fichier_graphe = sys.argv[1]
+    g = Graphe(fichier_graphe)
+    scarabees = []
+    for scarabee in sys.argv[2:]:
+        scarabees.append(Scarabee(g, scarabee))
